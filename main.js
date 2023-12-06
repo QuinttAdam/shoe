d453ddafa9fdf41f37e7999249bc562ea117269e
QuinttAdam <116001984+QuinttAdam@users.noreply.github.com>
2023-12-06 10:45:07 +0100

    feat: Update 3D model colors and add environment map

--- main.js
+++ main.js
@@ -16,12 +16,14 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 renderer.setClearColor(0xffffff); // Set the background color to white
 
-
 const controls = new OrbitControls(camera, renderer.domElement);
+
 
 // Create an instance of DRACOLoader
 const dracoLoader = new DRACOLoader();
-dracoLoader.setDecoderPath("https://www.gstatic.com/draco/versioned/decoders/1.5.6/"); // Set the path to the Draco decoder files
+dracoLoader.setDecoderPath(
+  "https://www.gstatic.com/draco/versioned/decoders/1.5.6/"
+); // Set the path to the Draco decoder files
 
 const loader = new GLTFLoader();
 loader.setDRACOLoader(dracoLoader); // Set DRACOLoader for the GLTFLoader
@@ -32,7 +34,6 @@ loader.load("public/models/Shoe_compressed.glb", (gltf) => {
   gltf.scene.position.set(0, 0, 0);
   gltf.scene.rotation.y = 1;
 
-
   shoe = gltf.scene.children[0];
   console.log(shoe);
 
@@ -41,57 +42,57 @@ loader.load("public/models/Shoe_compressed.glb", (gltf) => {
       if (child.name === "inside") {
         const colorPicker = document.getElementById("colorPickerInside");
         colorPicker.addEventListener("input", (event) => {
-        const selectedColor = event.target.value;
+          const selectedColor = event.target.value;
 
-        child.material.color.set(selectedColor);
+          child.material.color.set(selectedColor);
         });
       }
       if (child.name === "laces") {
         const colorPicker = document.getElementById("colorPickerLaces");
         colorPicker.addEventListener("input", (event) => {
-        const selectedColor = event.target.value;
+          const selectedColor = event.target.value;
 
-        child.material.color.set(selectedColor);
+          child.material.color.set(selectedColor);
         });
       }
       if (child.name === "outside_1") {
         const colorPicker = document.getElementById("colorPickerOutside_1");
         colorPicker.addEventListener("input", (event) => {
-        const selectedColor = event.target.value;
+          const selectedColor = event.target.value;
 
-        child.material.color.set(selectedColor);
+          child.material.color.set(selectedColor);
         });
       }
       if (child.name === "outside_2") {
         const colorPicker = document.getElementById("colorPickerOutside_2");
         colorPicker.addEventListener("input", (event) => {
-        const selectedColor = event.target.value;
+          const selectedColor = event.target.value;
 
-        child.material.color.set(selectedColor);
+          child.material.color.set(selectedColor);
         });
       }
       if (child.name === "outside_3") {
         const colorPicker = document.getElementById("colorPickerOutside_3");
         colorPicker.addEventListener("input", (event) => {
-        const selectedColor = event.target.value;
+          const selectedColor = event.target.value;
 
-        child.material.color.set(selectedColor);
+          child.material.color.set(selectedColor);
         });
       }
       if (child.name === "sole_bottom") {
         const colorPicker = document.getElementById("colorPickerSole_bottom");
         colorPicker.addEventListener("input", (event) => {
-        const selectedColor = event.target.value;
+          const selectedColor = event.target.value;
 
-        child.material.color.set(selectedColor);
+          child.material.color.set(selectedColor);
         });
       }
       if (child.name === "sole_top") {
         const colorPicker = document.getElementById("colorPickerSole_top");
         colorPicker.addEventListener("input", (event) => {
-        const selectedColor = event.target.value;
+          const selectedColor = event.target.value;
 
-        child.material.color.set(selectedColor);
+          child.material.color.set(selectedColor);
         });
       }
     }
@@ -101,7 +102,19 @@ loader.load("public/models/Shoe_compressed.glb", (gltf) => {
 });
 
 
-camera.position.z = 0.3;
+const cubeTextureLoader = new THREE.CubeTextureLoader();
+const environmentMapTexture = cubeTextureLoader.load([
+  "/cubemap/px.png",
+  "/cubemap/nx.png",
+  "/cubemap/py.png",
+  "/cubemap/ny.png",
+  "/cubemap/pz.png",
+  "/cubemap/nz.png",
+]);
+//add to background
+scene.background = environmentMapTexture;
+
+camera.position.z = 0.5;
 camera.position.y = 0.2;
 camera.lookAt(0, 0, -0.1);
 
